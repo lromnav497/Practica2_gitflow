ea0c0316a8a1432c80304e0a8e816bb7a02591b3
Jaime <jaimepb-94@hotmail.com>
2024-11-25 11:04:59 +0100

    Corregir conflictos y errores durante pruebas

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
 # main.py
-
-from operaciones import sumar, restar, multiplicar, dividir
-
-def main():
-    # Ejemplo de uso de las funciones
-    try:
-        num1 = 10
-        num2 = 5
-
-        print("Suma:", sumar(num1, num2))
-        print("Resta:", restar(num1, num2))
-        print("Multiplicación:", multiplicar(num1, num2))
-        print("División:", dividir(num1, num2))  # Llamada a la nueva función
-
-    except ValueError as e:
-        print(f"Error: {e}")
-
 from menu import mostrar_menu
-
-def main():
-    opcion, numero = mostrar_menu()
-    if opcion:
-        print(f"Has seleccionado la opción {opcion} y el número {numero}")
 
 
 if __name__ == "__main__":
-    main()
+    mostrar_menu()
